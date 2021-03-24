6f8636d27338a90557ad4287967457dbca621575
PhilMeshkovo <meshkovo1977@mail.ru>
2021-03-24 11:39:34 +0300

    delete condition in app

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ const [view, setView] = useState('main')
     <div className="App">
       <Header />
       <br/>
-      {view === 'main' && <Main view={view} setView={(props) => setView(props)}/>}
+      {/* {view === 'main' && <Main view={view} setView={(props) => setView(props)}/>}
       {view === 'sights' && <Sights view={view} setView={(props) => setView(props)}/>}
-      {view === 'history' && <History view={view} setView={(props) => setView(props)}/>}
+      {view === 'history' && <History view={view} setView={(props) => setView(props)}/>} */}
+      {view === 'main' ? <Main view={view} setView={(props) => setView(props)}/> : view !== 'history' ? 
+      <Sights view={view} setView={(props) => setView(props)}/> : <History view={view} setView={(props) => setView(props)}/>}
     </div>
   );
 }
