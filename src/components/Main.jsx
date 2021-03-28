673641c739eaf5437fb43bf5514aa9696f1dddd2
PhilMeshkovo <meshkovo1977@mail.ru>
2021-03-28 22:25:03 +0300

    add animation 700ms

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import Image from './pond.jpg'
 
-const Main = (props) => {
+function Main(props) {
 
-    const {setView = Function.prototype} = props;
+        const { setView = Function.prototype } = props;
 
-    const styles = {
-            height: 800,
-            backgroundImage: `url(${Image})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            width: `calc(100vw + 48px)`,
-            margin: -24,
-            padding: 24
-        
-    }
-    return <div id="Buttons" className="center pages" style={styles}>
-            <button className="btn waves-effect waves-light" onClick={() => setView('sights')} >Наши Достопримечательности</button>
-            <button className="btn waves-effect waves-light" onClick={() => setView('history')}>История Мешково</button>
-    </div>
+        const styles = {
+                height: 800,
+                backgroundImage: `url(${Image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                width: `calc(100vw + 48px)`,
+                margin: -24,
+                padding: 24
+        };
+        return <div id="Buttons" className="center pages" style={styles}>
+                <button className="btn waves-effect waves-light" onClick={() => setView('sights')}>Наши Достопримечательности</button>
+                <button className="btn waves-effect waves-light" onClick={() => setView('history')}>История Мешково</button>
+        </div>;
 }
 
 export {Main}
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header () {
     return <nav className="green darken-1 center">
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">MEШКОВО</a>
+      <a className="brand-logo">MEШКОВО</a>
       
     </div>
   </nav>
